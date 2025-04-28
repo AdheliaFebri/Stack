9de4fd2f17efd1bb4592f82d7c392bd5d4a6139a
AdheliaFebri <fullsunade2205@gmail.com>
2025-04-28 21:31:00 +0700

    Create Latihan1A.dart

---
+++ Latihan1A.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+
+class Stack<T> {
+  final List<T> _stack = [];
+
+  void push(T element) {
+    _stack.add(element);
+  }
+
+  T pop() {
+    if (_stack.isEmpty) {
+      throw Exception('Stack kosong');
+    }
+    return _stack.removeLast();
+  }
+
+  bool isEmpty() {
+    return _stack.isEmpty;
+  }
+
+  @override
+  String toString() => _stack.reversed.join('');
+}
+
+String convertToBase(int number, int base) {
+  Stack<int> stack = Stack<int>();
+
+  while (number > 0) {
+    stack.push(number % base);
+    number = number ~/ base;
+  }
+
+  if (stack.isEmpty()) {
+    return '0';
+  }
+
+  String result = '';
+  while (!stack.isEmpty()) {
+    int digit = stack.pop();
+    if (digit >= 10) {
+      result += String.fromCharCode(55 + digit); // 10 -> 'A', 11 -> 'B', etc.
+    } else {
+      result += digit.toString();
+    }   
+  }
+  return result;
+}
+
+void main() {
+  stdout.write("Masukkan nilai desimal: ");
+  int desimal = int.parse(stdin.readLineSync()!);
+
+  print("Hasil nilai biner       = ${convertToBase(desimal, 2)}");
+  print("Hasil nilai oktal       = ${convertToBase(desimal, 8)}");
+  print("Hasil nilai heksadesimal = ${convertToBase(desimal, 16)}");
+}
